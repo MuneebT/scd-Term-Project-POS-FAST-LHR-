dd4c28771e1370c6d82336ef807271752f5f7794
MuneebT <muneeb162002@gmail.com>
2024-10-23 12:09:34 +0500

    initial commit

---
+++ src/BranchManagementCode.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BranchManagementCode {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/branch", "root", "");
+            if (conn != null) {
+                System.out.println("Db connected");
+            } else {
+                System.out.println("Db not connected");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+//new BranchManagementForm();
+//BranchManagementForm bm=new BranchManagementForm();
+new UpdateScreen();
+
+    }
+}
