d4a59f1f909783292178ce7aeb37f43e4a9b427f
Syed Abdullah Hassan <syed.170804@gmail.com>
2024-11-09 23:57:10 +0500

    initial commit abdullah

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -2,12 +2,20 @@ package Controller;
 import Model.BranchManagementModel;
 import Model.UpdateScreenModel;
 import View.BranchManagementView;
+import View.CreateBranchView;
 import View.LoadingScreenView;
 import View.UpdateScreenView;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-new BranchManagementView();
+        try {
+            DBInitializer in=new DBInitializer();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        new CreateBranchView();
 //new UpdateScreenView();
 //BranchManagementModel.insert_data_into_db();
     }
