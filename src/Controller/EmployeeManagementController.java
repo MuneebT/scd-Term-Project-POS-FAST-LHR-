a29e942b894414fb7f1e4c2adf138bf3d6c58d80
Syed Abdullah Hassan <syed.170804@gmail.com>
2024-11-16 21:21:14 +0500

    add bm add screen and bm management screen with backend implementation

--- src/Controller/EmployeeManagementController.java
+++ src/Controller/EmployeeManagementController.java
@@ -3,19 +3,33 @@ package Controller;
 import Model.Employee;
 import Model.EmployeeDao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeManagementController {
     EmployeeDao employeeDao=new EmployeeDao();
     public EmployeeManagementController() {
     }
-    public void redirect_employee_insertion(String name,String empNo,String email,String branchCode,String salary,String designation)
+    public void redirect_employee_insertion(String name,String empNo,String password,String email,String branchCode,String salary,String designation)
     {
-        employeeDao.insertEmployee(name,empNo,email,branchCode,salary,designation);
+        //added password parameter here by abdullah
+        employeeDao.insertEmployee(name,empNo,email,password,branchCode,salary,designation);
     }
+
     public List<Employee> redirect_get_All_employees()
     {
         List<Employee> employees= employeeDao.getAllEmployees();
+        for(Employee employee:employees)
+        {
+            System.out.println(employee.name+" "+employee.email);
+        }
+        return employees;
+    }
+
+
+    public List<Employee> redirect_get_All_BMs() throws SQLException {
+
+        List<Employee> employees= employeeDao.getAllBMs();
         for(Employee employee:employees)
         {
             System.out.println(employee.name+" "+employee.email);
